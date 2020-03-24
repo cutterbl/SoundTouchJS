8c75d5007262bcf175285ddc4aa5d5f996937dd3
Blades, Stephen <Stephen.Blades@ul.com>
2020-03-24 05:59:38 -0500

    chore: update dependencies

--- dist/soundtouch.js
+++ dist/soundtouch.js
@@ -1,5 +1,5 @@
 /*
- * SoundTouch JS v0.1.7 audio processing library
+ * SoundTouch JS v0.1.8 audio processing library
  * Copyright (c) Olli Parviainen
  * Copyright (c) Ryan Berdeen
  * Copyright (c) Jakub Fiala
