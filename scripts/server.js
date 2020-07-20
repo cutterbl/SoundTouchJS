92cadaea52c4b074f011d4ca2ddcb06a81217fbd
Blades, Stephen <Stephen.Blades@ul.com>
2020-07-20 12:12:01 -0500

    chore: update dependencies

--- scripts/server.js
+++ scripts/server.js
@@ -1,10 +1,14 @@
 const connect = require('connect');
 const path = require('path');
 const serveStatic = require('serve-static');
+const open = require('open');
+const port = 8080;
+
 connect()
   .use(serveStatic(path.join(__dirname, '../public')))
   .use(serveStatic(path.join(__dirname, '../dist')))
-  .listen(8080, function () {
+  .listen(port, function () {
     console.log('dir is ', path.join(__dirname, '../public'));
-    console.log('Go to http://localhost:8080');
+    console.log(`Listing on http://localhost:${port}`);
+    open(`http://localhost:${port}`);
   });
